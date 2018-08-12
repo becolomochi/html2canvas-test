0d22cc41d1737505130a1d897a4ea941108bccb0
becolomochi <becolomochi@gmail.com>
2018-08-12 23:27:56 +0900

    できた

--- src/js/script.js
+++ src/js/script.js
@@ -4,22 +4,24 @@ import html2canvas from 'html2canvas';
 function capture() {
   html2canvas(document.getElementById('target')).then(function(canvas) {
     // document.body.appendChild(canvas);
-    // downloadImage(canvas.toDataURL());
+    downloadImage(canvas.toDataURL());
   });
 }
 
 function downloadImage(data) {
-  // let fname = 'capture_' + new Date()+ ".png";
-  // let encdata = atob(data.replace(/^.*,/, ''));
-  // let outdata = new Uint8Array(encdata.length);
-  // for (let i = 0; i < encdata.length; i++) {
-  //   outdata[i] = encdata.charCodeAt(i);
-  // }
-  // let blob = new Blob([outdata], ["image/png"]);
+  let fname = 'capture_' + new Date()+ ".png";
+  let encdata = atob(data.replace(/^.*,/, ''));
+  let outdata = new Uint8Array(encdata.length);
+  for (let i = 0; i < encdata.length; i++) {
+    outdata[i] = encdata.charCodeAt(i);
+  }
+  let blob = new Blob([outdata], ["image/png"]);
   
-  // document.getElementById("getImage").href = data;			//base64そのまま設定
-  // document.getElementById("getImage").download = fname;		//ダウンロードファイル名設定
-  // // document.getElementById("getImage").click(); 			//自動クリック
+  document.getElementById("getImage").href = data;			//base64そのまま設定
+  document.getElementById("getImage").download = fname;		//ダウンロードファイル名設定
+  document.getElementById("getImage").click(); 			//自動クリック
 }
 
-document.getElementById('button').onclick = capture();
+document.getElementById('button').onclick = () => {
+  capture();
+}
